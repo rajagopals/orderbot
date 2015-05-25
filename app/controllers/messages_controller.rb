df91aac0fcf222898b532d43ca599f3bbab1ba56
Rajagopal Sathyamurthi <rajagopals@gmail.com>
2015-05-24 22:58:36 -0700

    some checks on params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,16 @@
 require 'nexmo'
 class MessagesController < ApplicationController
 
-  def new
-    Message.create!(:content => params[:text], :from => params[:msisdn])
+  def incoming
+    if params[:text].present? and params[:msisdn].present?
+      Message.create!(:content => params[:text], :from => params[:msisdn])
 
-    nexmo = Nexmo::Client.new(key: '5dc9c08c', secret: 'e2691853')
+      nexmo = Nexmo::Client.new(key: '5dc9c08c', secret: 'e2691853')
 
-    nexmo.send_message(from: '12192099266', 
-                       to: params[:msisdn], 
-                       text: "You sent #{params[:text]}")
+      nexmo.send_message(from: '12192099266', 
+                         to: params[:msisdn], 
+                         text: "You sent #{params[:text]}")
+    end
 
     head :ok, content_type: "text/html"
   end
