a4dff52fa1ef53d5e015d19b2989c33fd710533d
Rajagopal Sathyamurthi <rajagopals@gmail.com>
2015-05-24 23:39:11 -0700

    smart regex captures

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,23 @@ class MessagesController < ApplicationController
 
       nexmo = Nexmo::Client.new(key: '5dc9c08c', secret: 'e2691853')
 
+      # http://stackoverflow.com/questions/546220/how-to-match-the-first-word-after-an-expression-with-regex
+      # http://stackoverflow.com/questions/7124778/how-to-match-anything-up-until-this-sequence-of-characters-in-a-regular-expres
+
+      # Capture everything until first occurance of 'from' or 'at' or the end of the sentence
+      if match = params[:text].match(/(?<=\bwant\s|\border\s)(.+?)(?=\bfrom|$|\bat)/i)
+        wants = match.captures.first || "nothing"
+      end
+
+
+      # Capture everything until first occurance of 'want' or 'order' or the end of the sentence
+      if match = params[:text].match(/(?<=\bfrom\s|\bat\s)(.+?)(?=\bwant|$|\border)/i)
+        from = match.captures.first || "I don't know where"
+      end
+
       nexmo.send_message(from: '12192099266', 
                          to: params[:msisdn], 
-                         text: "You sent #{params[:text]}")
+                         text: "Thanks for using OrderBOT! Your order of #{wants} from #{from} will be ready in 15 minutes!")
     end
 
     head :ok, content_type: "text/html"
